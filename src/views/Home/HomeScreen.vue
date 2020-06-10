8eea839d47e379c959fabe9d40af862b66e0291d
Rodolfo Santos <rodolfodossantos29@gmail.com>
2020-06-10 17:56:53 -0300

    Refatorando código

--- src/views/Home/HomeScreen.vue
+++ src/views/Home/HomeScreen.vue
@@ -2,28 +2,30 @@
   <div>
     <nav-menu></nav-menu>
     <b-container>
-      <b-row class="mt-4 exams-container">
-        <b-col md="4" class="p-2" v-for="exam in exams" :key="exam.id">
-          <div class="test-block p-4">
-            <div>
-              <h3>{{exam.title}}</h3>
-              <p>{{exam.description}}</p>
-              <hr />
+      <transition appear>
+        <b-row class="mt-4 exams-container">
+          <b-col md="4" class="p-2" v-for="exam in exams" :key="exam.id">
+            <div class="test-block p-4">
+              <div>
+                <h3>{{exam.title}}</h3>
+                <p>{{exam.description}}</p>
+                <hr />
+              </div>
+              <div class="text-muted">
+                <p>
+                  <strong>Autor:</strong>
+                  {{exam.author}}
+                </p>
+                <p>
+                  <strong>Nº Questões:</strong>
+                  {{ exam.questions.length }}
+                </p>
+                <b-button>Responder</b-button>
+              </div>
             </div>
-            <div class="text-muted">
-              <p>
-                <strong>Autor:</strong>
-                {{exam.author}}
-              </p>
-              <p>
-                <strong>Nº Questões:</strong>
-                {{ exam.questions.length }}
-              </p>
-              <b-button>Responder</b-button>
-            </div>
-          </div>
-        </b-col>
-      </b-row>
+          </b-col>
+        </b-row>
+      </transition>
     </b-container>
   </div>
 </template>
